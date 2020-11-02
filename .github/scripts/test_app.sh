ca9d5f30266f909b48dc51c91f682eda3f1d383c
phan.duong.ngoc <phan.duong.ngoc@sun-asterisk.com>
2020-11-02 14:15:51 +0700

    fix new

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -3,7 +3,7 @@
 
 set -eo pipefail
 
-xcodebuild -workspace DemoSi.xcodeproj \
+xcodebuild -xcodeproj DemoSi.xcodeproj \
             -scheme DemoSi\ iOS \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
